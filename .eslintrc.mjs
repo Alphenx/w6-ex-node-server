ddcea844cf918f284e5426ee68af479793ca2334
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-02-21 16:53:44 +0100

    Rename eslint from js to mjs

---
+++ .eslintrc.mjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "xo",
+  overrides: [
+    {
+      extends: ["xo-typescript"],
+      files: ["*.ts", "*.tsx"],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  ignorePatterns: ["**/public/**/*.js"],
+  rules: {
+    indent: ["Error", 2],
+    "object-curly-spacing": ["Error", "always"],
+  },
+  module: {
+    exports: {
+      extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      root: true,
+    },
+  },
+};
