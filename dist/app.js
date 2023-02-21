f7e7807430e081186a80680368115dba8761edd0
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-02-21 16:45:00 +0100

    Change ignore patterns from eslint and format app.js

--- dist/app.js
+++ dist/app.js
@@ -2,9 +2,9 @@ import express from 'express';
 const app = express();
 const port = 3000;
 app.get('/hello', (req, res) => {
-    res.json();
+  res.json();
 });
 app.post('/', (req, res) => {
-    res.send('hello im posting');
+  res.send('hello im posting');
 });
 app.listen(port);
