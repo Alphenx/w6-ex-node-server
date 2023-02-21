4846f6ed4b60f87258b9b13e2908eb7c1b785b78
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-02-21 13:46:10 +0100

    Initial commit

---
+++ app.ts
@@ -0,0 +1,17 @@
+// const os = require('node:os'); //COMMON JS -> CJS
+
+import os from 'node:os';
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/hello', (req, res) => {
+  res.json();
+});
+
+app.post('/', (req, res) => {
+  res.send('hello im posting');
+});
+
+app.listen(port);
